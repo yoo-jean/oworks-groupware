26e189257a6a2f17450b2261abf562c4898aa1c7
suuuinkim <suchae7@naver.com>
2021-06-11 11:01:44 +0900

    add : notice 클래스 및 인터페이스 추가
    add : notice 클래스 및 인터페이스 추가
    author : suinkim

---
+++ oworksProject/src/main/java/com/kh/oworks/board/model/service/NoticeServiceImpl.java
@@ -0,0 +1,5 @@
+package com.kh.oworks.board.model.service;
+
+public class NoticeServiceImpl {
+
+}
---
+++ oworksProject/src/main/java/com/kh/oworks/board/model/vo/Notice.java
@@ -0,0 +1,26 @@
+package com.kh.oworks.board.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public class Notice {
+	private int noticeNo;
+	private String empNo;
+	private String noticeTitle;
+	private String noticeContent;
+	private Date enrollDate;
+	private Date modifyDate;
+	private int noticeCount;
+	private String noticeStatus;
+}
